7ffd795be92f2197505f95a281f774460e7c373b
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-01-28 22:06:48 +0100

    add: keybindings for lsp renaming and window navigation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,9 @@
 vim.hl = vim.highlight
 vim.g.mapleader = " "
 print("Hello from init.lua")
-require("config.lazy")
 
 require("config.vim-keymaps").setup()
 require("config.vim-options").setup()
 require("config.vim-autocmds").setup()
+
+require("config.lazy")
--- nvim/.config/nvim/lua/config/vim-keymaps.lua
+++ nvim/.config/nvim/lua/config/vim-keymaps.lua
@@ -4,6 +4,11 @@ return {
     vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
     vim.keymap.set("n", "<leader>x", ":.lua<CR>")
     vim.keymap.set("v", "<leader>x", ":lua<CR>")
+    vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+    vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+    vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+    vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
     -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- disabled for neo-tree
   end,
 }
