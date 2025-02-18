01a422a18e75779d7652ac25a641e5f0bb000a52
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-02-18 10:55:47 +0100

    Changes: packages-setup-arch sudo in front of pacman

--- packages-setup-arch.sh
+++ packages-setup-arch.sh
@@ -3,6 +3,7 @@ sudo pacman -S git
 git config --global user.email "benedict.smit@tu-dortmund.de"
 git config --global user.name "Benedict Christian Smit"
 git config --global pull.rebase true
+git config --global init.defaultBranch main
 
 ### paru setup
 rustup default stable
@@ -14,72 +15,76 @@ cd ..
 rm -r paru
 
 ### fonts
-pacman -S ttf-meslo-nerd
-pacman -S ttf-font-awesome
-pacman -S ttf-roboto
-pacman -S adobe-source-sans-fonts
+sudo pacman -S ttf-meslo-nerd
+sudo pacman -S ttf-font-awesome
+sudo pacman -S ttf-roboto
+sudo pacman -S adobe-source-sans-fonts
 
 ### dotfile management
-pacman -S stow
+sudo pacman -S stow
 
 ### shell setup
-pacman -S zsh
+sudo pacman -S zsh
 
 ##### wm setup
 
 ### hyprland setup
-pacman -S wayland
-pacman -S hyprland
-pacman -S wofi
-pacman -S hyprpaper
-pacman -S hyprlock
-pacman -S hypridle
-pacman -S hyprshot
-pacman -S waybar
-pacman -S pavucontrol
+sudo pacman -S wayland
+sudo pacman -S hyprland
+sudo pacman -S wofi
+sudo pacman -S hyprpaper
+sudo pacman -S hyprlock
+sudo pacman -S hypridle
+sudo pacman -S waybar
+sudo pacman -S pavucontrol
+paru -S hyprshot
 
 ### terminal setup
-pacman -S starship
-pacman -S alacritty
-pacman -S tmux
-pacman -S tmuxp
+sudo pacman -S starship
+sudo pacman -S alacritty
+sudo pacman -S tmux
+sudo pacman -S tmuxp
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ### editor setup
-pacman -S neovim
+sudo pacman -S neovim
 
 ### browser setup
-pacman -S firefox
+sudo pacman -S firefox
 
 ### programs
-pacman -S signal-desktop
-pacman -S telegram-desktop
-pacman -S typst
+sudo pacman -S signal-desktop
+sudo pacman -S telegram-desktop
+sudo pacman -S typst
 
 ### terminal tools
-pacman -S ripgrep
-pacman -S bat
-pacman -S bat-extras
-pacman -S fd
-pacman -S fzf
-pacman -S lazygit
-pacman -S man
-pacman -S wl-clipboard
-pacman -S wget
+sudo pacman -S ripgrep
+sudo pacman -S bat
+sudo pacman -S bat-extras
+sudo pacman -S fd
+sudo pacman -S fzf
+sudo pacman -S lazygit
+sudo pacman -S man
+sudo pacman -S wl-clipboard
+sudo pacman -S wget
+
+### js & wasm stuff
+sudo pacman -S nodejs
+sudo pacman -S npm
+sudo pacman -S pnpm
+sudo pacman -S wasm-pack
 
 ### tree-sitter-cli
-pacman -S nodejs
-pacman -S npm
-pacman -S tree-sitter-cli
+sudo pacman -S tree-sitter-cli
 sudo npm install -g tree-sitter-cli
 
 ### make zsh as default shell
 chsh -s /bin/zsh
 
 ### i3 setup
-# pacman -S i3-wm
-# pacman -S polybar
-# pacman -S picom
-# pacman -S feh
-# pacman -S rofi
+# sudo pacman -S i3-wm
+# sudo pacman -S polybar
+# sudo pacman -S picom
+# sudo pacman -S feh
+# sudo pacman -S rofi
 
