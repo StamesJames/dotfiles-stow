96370e50600f894ce972389184aa263c002aff58
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-02-18 10:47:58 +0100

    Adds: Keymaps

--- nvim/.config/nvim/lua/config/vim-keymaps.lua
+++ nvim/.config/nvim/lua/config/vim-keymaps.lua
@@ -1,5 +1,7 @@
 return {
   setup = function()
+    -- terminal stuff
+    vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
     -- keymaps
     vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
     vim.keymap.set("n", "<leader>x", ":.lua<CR>")
@@ -9,6 +11,19 @@ return {
     vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
     vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-    vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- disabled for neo-tree
+    -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- deactivated for oil
+
+    -- move lines
+    vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+    vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+    -- scrolling
+    vim.keymap.set("n", "<C-d>", "<C-d>zz")
+    vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+    -- pasteing and deleting
+    vim.keymap.set("x", "<leader>p", '"_dP')
+    vim.keymap.set("n", "<leader>d", '"_dP')
+    vim.keymap.set("v", "<leader>d", '"_dP')
   end,
 }
