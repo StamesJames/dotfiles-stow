9988d18897bb57a47def018c2cff452036f1fa58
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-05-26 22:09:36 +0200

    change: make nvim lsp hover window with border

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -83,7 +83,9 @@ return {
       vim.diagnostic.config({
         float = { border = "single" },
       })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "K", function()
+        vim.lsp.buf.hover({ border = "rounded" })
+      end, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
