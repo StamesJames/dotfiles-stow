a804cb78aac417250557ff20ea9c42ee4fd5ded1
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-06-17 15:25:31 +0200

    Add: Godot aliases to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,3 +5,13 @@ if status is-interactive
   fish_vi_key_bindings
 end
 
+### my aliases
+alias godot4_3="$HOME/programs/godot/Godot_v4.3-stable_linux.x86_64"
+alias godot4_4="$HOME/programs/godot/Godot_v4.4-stable_linux.x86_64"
+alias godot4_4_1="$HOME/programs/godot/Godot_v4.4.1-stable_linux.x86_64"
+
+alias godot="godot4_4_1"
+alias gvim="nvim --listen 127.0.0.1:6004"
+alias vim="nvim"
+alias tvim="tmuxp load nvim-term"
+alias ls="ls --color=auto"
