1ec2732043949683addcfd6449c66b8b9a92bea8
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-07-30 08:05:09 +0200

    Fix: waybar weather now reads the right file

--- fish/.config/fish/functions/get_weather_waybar.fish
+++ fish/.config/fish/functions/get_weather_waybar.fish
@@ -1,4 +1,8 @@
 function get_weather_waybar
     curl wttr.in/\?format=1 >~/tmp/tmp_weather_save.txt 2>/dev/null
-    cat tmp_weather_save.txt
+    if test -e ~/tmp/tmp_weather_save.txt
+        cat ~/tmp/tmp_weather_save.txt
+    else
+        echo "no data"
+    end
 end
