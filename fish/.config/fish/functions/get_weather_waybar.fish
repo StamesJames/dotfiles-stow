a98bfe57d2e09652be30591bc7f0a424f4f0f972
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-08-26 11:29:58 +0200

    Add: Check if tmp dictionary exists for weather data curl

--- fish/.config/fish/functions/get_weather_waybar.fish
+++ fish/.config/fish/functions/get_weather_waybar.fish
@@ -1,4 +1,7 @@
 function get_weather_waybar
+    if ! test -d ~/tmp
+        mkdir ~/tmp
+    end
     curl wttr.in/\?format=1 >~/tmp/tmp_weather_save.txt 2>/dev/null
     if test -e ~/tmp/tmp_weather_save.txt
         cat ~/tmp/tmp_weather_save.txt
