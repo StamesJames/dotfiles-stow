456e07fb4e2a671cf1526745d6fc9a935048dd03
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-09-06 07:59:36 +0200

    Add: nvim documentation for my telescope commands

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -27,19 +27,25 @@ return {
       local live_multigrep = require("config.telescope.multigrep").live_multigrep
 
       -- key bindings
+      -- find in nvim help
       vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
+      -- find in currend workind directory
       vim.keymap.set("n", "<leader>fd", require("telescope.builtin").find_files)
+      -- grep in all files in the current working directory an with two spaces you can give a filter for files
       vim.keymap.set("n", "<leader>fg", live_multigrep)
+      -- find in nvim config
       vim.keymap.set("n", "<leader>en", function()
         require("telescope.builtin").find_files({
           cwd = vim.fn.stdpath("config"),
         })
       end)
+      -- This searches for all files in neovim plugin files
       vim.keymap.set("n", "<leader>ep", function()
         require("telescope.builtin").find_files({
           cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
         })
       end)
+      -- This searches in my notes folder
       vim.keymap.set("n", "<leader>fn", function()
         require("telescope.builtin").find_files({
           cwd = vim.fn.expand("~/Nextcloud/notes/"),
