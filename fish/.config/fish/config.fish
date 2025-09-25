a5e3822276e9b9173ad1dfebf917f388917e8381
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-09-25 16:18:36 +0200

    Add: config.fish globaly set EDITOR, GIT_EDITOR and VISUAL variable

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   starship init fish | source
   fish_vi_key_bindings
+  set --local editor nvim
+  set -gx EDITOR $editor
+  set -gx GIT_EDITOR $editor
+  set -gx VISUAL $editor
 end
 
 ### my aliases
@@ -15,4 +19,3 @@ alias gvim="nvim --listen 127.0.0.1:6004"
 alias vim="nvim"
 alias tvim="tmuxp load nvim-term"
 alias ls="ls --color=auto"
-
