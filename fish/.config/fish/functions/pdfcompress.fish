a9ac674572cb09bfb8f4d3f0d887d13342a7540b
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-12-09 20:12:27 +0100

    Add: fishfunction pdfcompress

---
+++ fish/.config/fish/functions/pdfcompress.fish
@@ -0,0 +1,5 @@
+function pdfcompress
+  for filename in $argv
+     gs -q -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dPDFSETTINGS=/screen -dEmbedAllFonts=true -dSubsetFonts=true -dColorImageDownsampleType=/Bicubic -dColorImageResolution=144 -dGrayImageDownsampleType=/Bicubic -dGrayImageResolution=144 -dMonoImageDownsampleType=/Bicubic -dMonoImageResolution=144 -sOutputFile="$filename".compressed.pdf "$filename"; 
+  end
+end
