67b11ceac9abdc30199e55326f19fc5bebcc14ec
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2026-01-19 09:53:37 +0100

    Remove: nvim test keybindings

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -6,10 +6,10 @@ return {
     },
     config = function(_, _)
       vim.keymap.set("n", "<leader>t", ":TestNearest<CR>")
-      vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
-      vim.keymap.set("n", "<leader>a", ":TestSuit<CR>")
-      vim.keymap.set("n", "<leader>l", ":TestLast<CR>")
-      vim.keymap.set("n", "<leader>g", ":TestVisit<CR>")
+      -- vim.keymap.set("n", "<leader>T", ":TestFile<CR>")
+      -- vim.keymap.set("n", "<leader>a", ":TestSuit<CR>")
+      -- vim.keymap.set("n", "<leader>l", ":TestLast<CR>")
+      -- vim.keymap.set("n", "<leader>g", ":TestVisit<CR>")
       vim.cmd("let test#stratagy = 'vimux'")
     end,
   },
