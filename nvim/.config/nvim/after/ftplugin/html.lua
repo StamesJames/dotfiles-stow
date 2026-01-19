f1bce797a4a367e2a580d39d2c3ebb66fb70eaeb
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2026-01-19 09:53:08 +0100

    Change: nvim html ftplugin

---
+++ nvim/.config/nvim/after/ftplugin/html.lua
@@ -0,0 +1,9 @@
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 0
+vim.opt_local.shiftwidth = 2
+vim.opt_local.expandtab = true
+
+vim.opt_local.autoindent = true
+vim.opt_local.smartindent = true
+vim.opt_local.cindent = false
+vim.opt_local.indentexpr = ""
