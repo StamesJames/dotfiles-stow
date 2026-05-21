8b1c035a0009974c8fa4f665f7c42ca9e9b44577
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2026-05-21 12:19:01 +0200

    Fix: nvim treesitter problems

---
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {
+      auto_install = true,
+      highlight = false,
+    },
+  },
+}
