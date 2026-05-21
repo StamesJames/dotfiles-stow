9b276611727977e02555cb286cc39cf02eee5087
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2026-05-21 12:20:54 +0200

    Add: nvim ts-autotag disabled for rust and lean

--- nvim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -8,6 +8,18 @@ return {
         enable_rename = true,          -- Auto rename pairs of tags
         enable_close_on_slash = false, -- Auto close on trailing </
       },
+      per_filetype = {
+        ["rust"] = {
+          enable_close = false,
+          enable_rename = false,
+          enable_close_on_slash = false,
+        },
+        ["lean"] = {
+          enable_close = false,
+          enable_rename = false,
+          enable_close_on_slash = false,
+        },
+      },
     },
   },
 }
